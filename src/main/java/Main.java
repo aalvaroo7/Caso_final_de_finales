4ddcd233d4a371df57c9be7dcf6113e96ec3effe
aalvaroo7 <almarrom@myuax.com>
2024-05-08 10:04:53 +0200

    codigo ejercicio 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = recursive(1, 2);
+        System.out.println(a);
+    }
+
+    public static int recursive(int a, int b) {
+        if (b == 0) {
+            return 1;
+        } else if (a == 0) {
+            return 0;
+        } else {
+            return a * recursive(a, b - 1);
+        }
     }
 }

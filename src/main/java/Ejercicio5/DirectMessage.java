bd3d9f24b4a0f1abde2c80d108adc2e900586ed3
aalvaroo7 <almarrom@myuax.com>
2024-05-08 10:40:43 +0200

    ejericio 5

---
+++ src/main/java/Ejercicio5/DirectMessage.java
@@ -0,0 +1,12 @@
+package Ejercicio5;
+
+public class DirectMessage extends Tweet {
+    private UserAccount receiver;
+
+    public DirectMessage(String message, UserAccount sender, UserAccount receiver) {
+        super(message, sender);
+        this.receiver = receiver;
+    }
+
+    // ... getters and setters ...
+}

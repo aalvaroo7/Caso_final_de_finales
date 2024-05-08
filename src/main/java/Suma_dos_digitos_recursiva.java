71ac0e4e92fd32e70d729163833319d9f6cd3a76
aalvaroo7 <almarrom@myuax.com>
2024-05-08 10:11:52 +0200

    explicacion 3 + planteamiento del codigo de la suma recursiva

---
+++ src/main/java/Suma_dos_digitos_recursiva.java
@@ -0,0 +1,15 @@
+public class Suma_dos_digitos_recursiva {
+    public static void main(String[] args) {
+        int number = 102;
+        int sum = sumOfDigits(number);
+        System.out.println(sum);
+    }
+
+    public static int sumOfDigits(int n) {
+        if (n < 10) {
+            return n;
+        } else {
+            return n % 10 + sumOfDigits(n / 10);
+        }
+    }
+}
